dcd159b1c091726441224454be2dccf7eef820e7
George Bryant <gb@gbryant.co.uk>
2013-11-21 15:27:38 +0000

    Added initial syntax definitions

--- noteparser/main.py
+++ noteparser/main.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+
 """
 
-NoteVelocity - A speedy note-taking program
+NoteParser - A simple Python script to parse my Note Markup Language
 Copyright (C) 2013  George Bryant
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -11,3 +13,26 @@ You should have received a copy of the GNU General Public License along with thi
 
 """
 
+# Imports
+
+# Definitions - allow me to change syntax later
+italicTag = "*"
+boldTag = "**"
+underlineTag = "_"
+strikethroughTag = "--"
+
+bulletTag = "* " 		# At start of line
+blockTag = "> " 	# At start of line
+quoteTags = ["\"\"\"", "\'\'\'", "```"]
+
+equationTags = ["[", "]"]
+exponentTags = ["^(", ")"]
+exponentSingleTag = "^"
+baseTags = ["_(", ")"]
+baseSingleTag = "_"
+
+symbolDict = {
+	"pi": "\03C0"
+}
+
+print(symbolDict["pi"])
